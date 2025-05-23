cde2cb03ac2ab484eea79297ea847ed05a51554b
Catalina-S <catalina.sanchez-m@mail.escuelaing.edu.co>
2025-05-22 22:02:52 -0500

    cambiomongo

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://mariajopineda:<a315-55g>@cluster0.7uchx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb+srv://catalinasanchezm:F0LmP240@samplepatientservice.khioc.mongodb.net/?retryWrites=true&w=majority&appName=SamplePatientService"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
