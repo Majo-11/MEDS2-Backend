604a3d854326081158b33c818b5d755d702504ac
Majo-11 <mariajo.pineda@urosario.edu.co>
2025-05-22 21:14:26 -0500

    0

--- connection.py
+++ connection.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 
-def connect_to_mongodb(MedicationRequest, Medication):
+def connect_to_mongodb(db_name, collection_name):
     uri = "mongodb+srv://mariajopineda:<a315-55g>@cluster0.7uchx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client[MedicationRequest]
-    collection = db[Medication]
+    db = client[db_name]
+    collection = db[collection_name]
     return collection
